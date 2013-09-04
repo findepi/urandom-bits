5862fe30d9065244f27a4df5a0de9d80fc068378
Piotr Findeisen <piotr.findeisen@syncron.com>
2013-09-04 10:02:40 +0200

    ReadProperties

---
+++ src/main/java/piofin/test/properties/ReadProperties.java
@@ -0,0 +1,17 @@
+package piofin.test.properties;
+
+import java.io.IOException;
+import java.util.Properties;
+
+/**
+ * @author piofin <piotr.findeisen@syncron.com>
+ * @since Sep 4, 2013
+ */
+public class ReadProperties {
+	public static void main(String[] args) throws IOException {
+		Properties p = new Properties();
+		p.load(ReadProperties.class.getResourceAsStream("prop1.properties"));
+
+		System.out.println("" + p);
+	}
+}
