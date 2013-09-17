1e8a88a92435cdc23d70ae016a9dbec3d8ece304
Piotr Findeisen <piotr.findeisen@syncron.com>
2013-09-17 12:56:02 +0200

    Example of package private method call from other package.

---
+++ src/main/java/piofin/test/packages/packageprivate/packagea/ppclass.java
@@ -0,0 +1,16 @@
+package piofin.test.packages.packageprivate.packagea;
+
+/**
+ * @author piofin <piotr.findeisen@syncron.com>
+ * @since Sep 17, 2013
+ */
+class ppclass {
+
+	public Gizmo foo() {
+		// TODO Auto-generated method stub
+return null;
+	}
+
+	private class Gizmo {
+	}
+}
---
+++ src/main/java/piofin/test/packages/packageprivate/packagea/publicclass.java
@@ -0,0 +1,12 @@
+package piofin.test.packages.packageprivate.packagea;
+
+/**
+ * @author piofin <piotr.findeisen@syncron.com>
+ * @since Sep 17, 2013
+ */
+public class publicclass extends ppclass {
+
+	private void callf() {
+		foo();
+	}
+}
