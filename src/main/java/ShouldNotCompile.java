123dfe3cf2169122542cc984e35315b8ced4c90c
Piotr Findeisen <piotr.findeisen@syncron.com>
2015-12-31 17:37:57 +0100

    ShouldNotCompile for eclipse bug 485057
    https://bugs.eclipse.org/bugs/show_bug.cgi?id=485057

---
+++ src/main/java/ShouldNotCompile.java
@@ -0,0 +1,12 @@
+public class ShouldNotCompile {
+
+	public static void main(String[] args) {
+		foo(rt -> true); // javac 1.8.0_66: error: reference to foo is ambiguous
+	}
+
+	public static <T extends java.io.Serializable> void foo(T serialiable) {
+	}
+
+	public static void foo(java.util.function.Predicate<?> predicate) {
+	}
+}
