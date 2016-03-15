fbf9197d77f015b90451ce12c84614d4bad72a3b
Piotr Findeisen <piotr.findeisen@syncron.com>
2016-03-16 00:41:43 +0100

    Alloc1M

---
+++ src/main/java/piofin/test/java/mem/Alloc1M.java
@@ -0,0 +1,27 @@
+package piofin.test.java.mem;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Run with e.g.
+ *
+ * <pre>
+ * -XX:-CITime  -XX:+PrintCompilation -Xint
+ * </pre>
+ *
+ * @author piofin <piotr.findeisen@syncron.com>
+ * @since Mar 16, 2016
+ */
+public class Alloc1M {
+	public static void main(String[] args) {
+		final int count = 1_000_000;
+		long start = System.currentTimeMillis();
+		List<Object> list = new ArrayList<>(/* count */);
+		for (int i = 0; i < count; ++i) {
+			list.add(new Object());
+		}
+		long end = System.currentTimeMillis();
+		System.out.printf("took %s ms\n", end - start);
+	}
+}
