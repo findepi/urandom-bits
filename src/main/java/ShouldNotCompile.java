6423d70be2857b1cce42befe9b7e6758a0ecbbfb
Piotr Findeisen <piotr.findeisen@syncron.com>
2016-10-12 14:29:51 +0200

    Comment out (intentional) compile error
    IDE complains too loudly.

--- src/main/java/ShouldNotCompile.java
+++ src/main/java/ShouldNotCompile.java
@@ -1,7 +1,7 @@
 public class ShouldNotCompile {
 
 	public static void main(String[] args) {
-		foo(rt -> true); // javac 1.8.0_66: error: reference to foo is ambiguous
+//		foo(rt -> true); // javac 1.8.0_66: error: reference to foo is ambiguous
 	}
 
 	public static <T extends java.io.Serializable> void foo(T serialiable) {
