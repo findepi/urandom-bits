45930d75e35c330fd60185f122f302d8b0b7f6f4
Piotr Findeisen <piotr.findeisen@gmail.com>
2020-07-28 13:39:29 +0200

    Add 64-bit timestamp max with nano precision

--- src/main/java/findepi/time/LongTimestampRange.java
+++ src/main/java/findepi/time/LongTimestampRange.java
@@ -6,8 +6,10 @@ public class LongTimestampRange
 {
     public static void main(String[] args)
     {
-        System.out.println("Max with contiguous encoding, p=7:  " + Instant.ofEpochMilli(Long.MAX_VALUE / 10_000));
-        System.out.println("Max with contiguous encoding, p=6: " + Instant.ofEpochMilli(Long.MAX_VALUE / 1_000));
-        System.out.println("Max with fast millis             : " + Instant.ofEpochMilli(Long.MAX_VALUE >> 10));
+        System.out.println("64-bit max with contiguous encoding, p=9:       " + Instant.ofEpochMilli(Long.MAX_VALUE / 1_000_000));
+        System.out.println("64-bit max with contiguous encoding, p=7:     " + Instant.ofEpochMilli(Long.MAX_VALUE / 10_000));
+        System.out.println("64-bit max with contiguous encoding, p=6:    " + Instant.ofEpochMilli(Long.MAX_VALUE / 1_000));
+        System.out.println("64-bit max with contiguous encoding, p=3: " + Instant.ofEpochMilli(Long.MAX_VALUE));
+        System.out.println("64-bit max with fast millis             :    " + Instant.ofEpochMilli(Long.MAX_VALUE >> 10));
     }
 }
