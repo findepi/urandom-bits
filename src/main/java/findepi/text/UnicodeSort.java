25090cfc6ce3a17ee85c55ece2f11a59b7b0b105
Piotr Findeisen <piotr.findeisen@gmail.com>
2020-09-14 12:08:19 +0200

    Add UnicodeSort

---
+++ src/main/java/findepi/text/UnicodeSort.java
@@ -0,0 +1,65 @@
+package findepi.text;
+
+import com.google.common.collect.Ordering;
+import com.google.common.primitives.Ints;
+import io.airlift.slice.Slices;
+
+import java.nio.charset.StandardCharsets;
+import java.util.Comparator;
+import java.util.List;
+
+import static java.util.Objects.requireNonNull;
+
+public class UnicodeSort
+{
+    public static void main(String[] args)
+    {
+        testSort("abc", "abd");
+        testSort("ab⌘", "abc");
+        testSort("ab\uFAD8", "ab\uD83D\uDD74");
+    }
+
+    private static void testSort(String left, String right)
+    {
+        System.out.println();
+
+        compare(left, right, "String::compareTo", String::compareTo);
+
+        compare(left, right, "UTF-8 bytes unsigned", Ordering.<Integer>natural()
+                .lexicographical()
+                .onResultOf(UnicodeSort::getUtf8UnsignedBytes));
+
+        compare(left, right, "UTF-8 slice", Comparator.comparing(Slices::utf8Slice));
+    }
+
+    private static void compare(String left, String right, String comparatorName, Comparator<String> comparator)
+    {
+        requireNonNull(left, "left is null");
+        requireNonNull(right, "right is null");
+
+        int comparison = comparator.compare(left, right);
+        String result;
+        if (comparison < 0) {
+            result = "less than";
+        }
+        else if (comparison == 0) {
+            result = "equal to";
+        }
+        else {
+            result = "greater than";
+        }
+
+        System.out.printf("'%s' is %s '%s' according to %s\n", left, result, right, comparatorName);
+    }
+
+    private static List<Integer> getUtf8UnsignedBytes(String s)
+    {
+        requireNonNull(s, "s is null");
+        byte[] bytes = s.getBytes(StandardCharsets.UTF_8);
+        int[] unsignedBytes = new int[bytes.length];
+        for (int i = 0; i < bytes.length; i++) {
+            unsignedBytes[i] = 0xFF & bytes[i];
+        }
+        return Ints.asList(unsignedBytes);
+    }
+}
